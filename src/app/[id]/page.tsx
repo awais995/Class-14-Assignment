264b009414a568adb25e8812649c3a23533bf00f
Muhammmad Awais <awaissoomro65@gmail.com>
2024-12-24 15:39:14 +0500

    assignment complete

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,20 +1,48 @@
 const DynamicPost = async (props: any) => {
 
-    const url = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${props.params.id}`
+    // Fetch data from the posts API
+    const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${props.params.id}`);
+    const post = await postResponse.json();
 
-    );
-    const res = await url.json();
-    console.log("2", res)
+    // Fetch data from the comments API
+    const commentResponse = await fetch(`https://jsonplaceholder.typicode.com/comments/${props.params.id}`);
+    const comment = await commentResponse.json();
+
+    // Fetch data from the albums API
+    const albumResponse = await fetch(`https://jsonplaceholder.typicode.com/albums/${props.params.id}`);
+    const album = await albumResponse.json();
+
+    // Log the fetched data
+    console.log("Post:", post);
+    console.log("Comment:", comment);
+    console.log("Album:", album);
     return (
         <div>
-            <h1 className="pl-5 text-5xl">
-                ID:        {props.params.id} <br /> <br />
-                UserId:    {res.userId} <br /> <br />
-                Post:      {res.id} <br /> <br />
-                Title:     {res.title} <br /> <br />
-                Body:      {res.body}
-            </h1>
+            {/* Posts Data */}
+            <div className="pl-5 text-3xl">
+                <h1 className="pl-5 text-5xl py-10">Posts</h1>
+                <strong>UserId:</strong> {post.userId} <br /> <br />
+                <strong>ID:</strong> {post.id} <br /> <br />
+                <strong>Title:</strong> {post.title} <br /> <br />
+                <strong>Post:</strong> {post.body} <br /> <br />
+            </div>
+            {/* Comments Data */}
+            <div className="pl-5 text-3xl">
+                <h1 className="pl-5 text-5xl py-10">Comments</h1>
+                <strong>Post ID:</strong> {comment.postId} <br /> <br />
+                <strong>ID:</strong> {comment.id} <br /> <br />
+                <strong>User Name:</strong> {comment.name} <br /> <br />
+                <strong>User Email:</strong> {comment.email} <br /> <br />
+                <strong>Comment:</strong> {comment.body} <br /> <br />
+            </div>
+
+            {/* Albums Data*/}
+            <div className="pl-5 text-3xl">
+                <h1 className="pl-5 text-5xl py-10">Albums</h1>
+                <strong>User ID:</strong> {album.userId} <br /> <br />
+                <strong>ID:</strong> {album.id} <br /> <br />
+                <strong>Title:</strong> {album.title} <br /> <br />
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,63 @@
 import Link from "next/link";
 
 export default async function Home() {
-  const url = await fetch ("https://jsonplaceholder.typicode.com/posts");
-  const res = await url.json ();
-  console.log (res)
+  
+  const posturl = await fetch ("https://jsonplaceholder.typicode.com/posts"); // fetch data from an api
+  
+  const posts = await posturl.json (); //convert it into json format and store it into posts variable.
+
+  const commentsUrl = await fetch ("https://jsonplaceholder.typicode.com/comments") // fetch data from an api
+  
+  const comments = await commentsUrl.json(); //convert it into json format and store it into comments variable.
+
+  const albumsurl = await fetch ("https://jsonplaceholder.typicode.com/comments")// fetch data from an api
+
+  const albums = await albumsurl.json(); //convert it into json format and store it into albums variable.
+
+  // console all data to check it shows or not
+  console.log ("posts", posts)
+  console.log ("Comments", comments)
+  console.log ("Albums", albums)
   return (
     
     <main>
-        {res.map((Post:any, index:number) => (
+      {/* Posts */}
+      {/* used map function to iterate the array of objects and store it in to props argumnet and pass the index */}
+        {posts.map((Post:any, index:number) => (
           <div key ={index}>
-            <Link href={`${Post.id}`}>
-            <h1 className="ml-5 text-3xl text-wrap"> 
+            {/* link the post with id */}
+            <Link href={`${Post.id}`}> 
+            {/* shows on the browser */}
+            <h1 className="ml-5 text-3xl"> 
               {Post.id} &nbsp; &nbsp;
               {Post.title} </h1>
             </Link>
           </div>
-        ))}
+        ))};
+        {/* Comments */}
+      <h1 className="text-5xl ml-5 py-6">Comments</h1>
+        {comments.map((comment:any, index:number) => (
+          <div key={index}>
+            <Link href={`${comment.id}`}>
+            <h1 className="ml-5 text-3xl"> 
+              {comment.id} &nbsp; &nbsp;
+              {comment.name} &nbsp; &nbsp;
+            </h1>
+            </Link>
+          </div>
+        ))};
+          {/* albums */}
+        <h1 className="text-5xl ml-5 py-6"> Albums</h1>
+        {albums.map((album:any, index:number) => (
+          <div key={index}>
+              <Link href={`${album.id}`}>
+              <h1 className="ml-5 text-3xl">
+                {album.id} &nbsp; &nbsp;
+                {album.name} &nbsp; &nbsp;
+              </h1>
+              </Link>
+          </div>
+        ))};
     </main>
   );
 }
